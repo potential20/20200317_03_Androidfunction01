090b8fa8dcef3ca05870d953e6124b499a02be32
potential20 <passion212@nate.com>
2020-03-17 16:08:17 +0900

    첫번째 액티비티 추가

---
+++ app/src/main/java/kr/co/tjoeun/a20200317_03_androidfunction01/FirstActivity.java
@@ -0,0 +1,20 @@
+package kr.co.tjoeun.a20200317_03_androidfunction01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import kr.co.tjoeun.a20200317_03_androidfunction01.databinding.ActivityFirstBinding;
+
+public class FirstActivity extends AppCompatActivity {
+
+    ActivityFirstBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_first);
+
+    }
+}
