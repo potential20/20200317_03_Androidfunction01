2500d2fd6f7510b170771ccc3d915a395facc3de
potential20 <passion212@nate.com>
2020-03-17 16:15:07 +0900

    Intent를 이용한 화면 이동

--- app/src/main/java/kr/co/tjoeun/a20200317_03_androidfunction01/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200317_03_androidfunction01/MainActivity.java
@@ -3,6 +3,7 @@ package kr.co.tjoeun.a20200317_03_androidfunction01;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -20,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
         binding.goToFirstBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+//                FirstActivity로 이동하자!
+                Intent intent = new Intent(MainActivity.this, FirstActivity.class);
+                startActivity(intent);
 
             }
         });
