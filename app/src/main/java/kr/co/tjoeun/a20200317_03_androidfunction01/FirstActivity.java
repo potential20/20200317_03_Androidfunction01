93a8a82e08c520677a6e3a563e075663e82f74e0
potential20 <passion212@nate.com>
2020-03-17 16:10:22 +0900

    첫 액티비티의 버튼 / 클릭 이벤트 준비

--- app/src/main/java/kr/co/tjoeun/a20200317_03_androidfunction01/FirstActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200317_03_androidfunction01/FirstActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
 
 import kr.co.tjoeun.a20200317_03_androidfunction01.databinding.ActivityFirstBinding;
 
@@ -15,6 +16,13 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this,R.layout.activity_first);
+
+        binding.finishFirstBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
